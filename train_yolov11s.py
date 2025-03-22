d4fb0659021e7eda6ff24dbd95d3e13a4dd37267
amangupta20 <58002413+amangupta20@users.noreply.github.com>
2025-03-22 23:14:27 +0530

    changing training specs

--- train_yolov11s.py
+++ train_yolov11s.py
@@ -7,8 +7,8 @@ model = YOLO('yolo11m.pt')
 model.train(
     data='data/data.yaml',  # Path to your data.yaml file
     epochs=100,             # Number of training epochs
-    imgsz=640,              # Reduced image size to 640 (from 1280) to reduce memory
-    batch=12,               # Increased batch size to 12 (from 8) - user request
+    imgsz=1280,              # Reduced image size to 640 (from 1280) to reduce memory
+    batch=16,               # Increased batch size to 12 (from 8) - user request
     cache='ram',           # Cache dataset in RAM for faster training
     optimizer='AdamW',     # Optimizer to use (AdamW)
     lr0=0.001              # Initial learning rate
